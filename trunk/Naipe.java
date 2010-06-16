e305879f10b629e920fff04eb0c2a5a1bf2215dc
wanderlan.anjos <wanderlan.anjos@56f67149-3aac-0ede-8955-d749bc2e7fa0>
2010-06-16 20:49:58 +0000

    Melhorias em Naipe

--- trunk/Naipe.java
+++ trunk/Naipe.java
@@ -1,3 +1,14 @@
 public enum Naipe {
-  ESPADAS, PAUS, COPAS, OUROS 
+  ESPADAS("Espadas"), PAUS("Paus"), COPAS("Copas"), OUROS("Ouros");
+  private String valor;
+
+ 	private Naipe(String valor){
+ 		this.valor = valor;	
+ 	}
+ 	
+	@Override
+	public String toString(){
+		return this.valor;
+	}
+  
 }
