f92c4d831a2f45354b30f3ba94686c6075bca322
wanderlan.anjos <wanderlan.anjos@56f67149-3aac-0ede-8955-d749bc2e7fa0>
2010-06-17 14:08:46 +0000

    Nova classe Mão

--- trunk/BlackJack.java
+++ trunk/BlackJack.java
@@ -1,4 +1,7 @@
 public class BlackJack {
+	private double minAposta=1, maxAposta=100;
+	public int numJogadores=1;
+	
 	public static void main(String[] args) {
     Jogador jogador = new Jogador();
     Banca banca = new Banca();
--- trunk/Monte.java
+++ trunk/Monte.java
@@ -1,13 +1,40 @@
 public class Monte {
-	public static final int tamanho=8;
+	private int tamanho=8, c=0;
 	private Baralho baralhos[] = new Baralho[tamanho];
   private Carta cartas[] = new Carta[tamanho*Baralho.tamanho];
   
-	public void embaralhar() {
-
+	public Monte() {
+		super();
+		int k=0;
+		for(int i=0; i<baralhos.length; i++){
+			baralhos[i] = new Baralho();
+			for(int j=0; j<Baralho.tamanho; j++){
+				cartas[k] = baralhos[i].GetCarta(j);
+				k++;
+			}
+		}
 	}
 
-	public void descartarCarta() {
+	public void embaralhar() {
+		Carta temp;
+		for(int i=0, j; i<cartas.length%2; i++){
+			j = (int)Math.round(Math.random()*cartas.length);
+			temp=cartas[j];
+			cartas[j]=cartas[i];
+			cartas[i]=temp;
+		}
+		c=0;
+	}
 
+	public int getTamanho() {
+		return tamanho;
+	}
+
+	public void setTamanho(int tamanho) {
+		this.tamanho = tamanho;
+	}
+
+	public Carta getCarta() {
+		return cartas[c++];
 	}
 }
