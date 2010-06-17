8bab36ff5c9736f4ecd4a6a6959929ffe02363be
wanderlan.anjos <wanderlan.anjos@56f67149-3aac-0ede-8955-d749bc2e7fa0>
2010-06-17 14:57:52 +0000

    Mais implementações

--- trunk/BlackJack.java
+++ trunk/BlackJack.java
@@ -1,10 +1,11 @@
 public class BlackJack {
 	private double minAposta=1, maxAposta=100;
-	public int numJogadores=1;
+	public static int numJogadores=1;
 	
 	public static void main(String[] args) {
-    Jogador jogador = new Jogador();
+    Jogador jogadores[] = new Jogador[numJogadores];
     Banca banca = new Banca();
+    banca.setJogadores(jogadores);
     
 	}
 }
