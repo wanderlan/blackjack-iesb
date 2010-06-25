d4cdf2dffc82965c60b7d6cf97d93fee89fe33ac
wanderlan.anjos@gmail.com <wanderlan.anjos@gmail.com@56f67149-3aac-0ede-8955-d749bc2e7fa0>
2010-06-25 16:18:13 +0000

    Caminho feliz quase ok

--- trunk/BlackJack.java
+++ trunk/BlackJack.java
@@ -30,36 +30,41 @@ public class BlackJack implements UIJogador{
 	}
 	
 	public void fazerJogada(){
-    for(int i=0; i<jogadores.length; i++){
-			System.out.println("\nOpções do Jogador " + jogadores[i].getId());
-			System.out.println("(P)edir\n(F)icar\n(D)ividir\n(S)eguro\nF(i)nalizar\n");
-			char c = 'E'; // erro 
-			try{
-				if(jogadores[i].getTotal()==0)
-					c = 'I';
-				else
-					c = Character.toUpperCase((char)System.in.read());
-				switch(c){
-			  	case 'P': 
-			  		System.out.println("Nova carta: " + jogadores[i].pedirCarta().toString());
-			  		System.out.println("Valor da mão: " + jogadores[i].getMao().getValor());
-			  		break;
-			  	case 'F':
-			  		System.out.println("Valor da mão: " + jogadores[i].getMao().getValor());
-			  		break;		  		
-			  	case 'D':
-			  		break;
-			  	case 'S':
-			  		break;
-			  	case 'I':
-			  		System.out.println("Obrigado por participar Jogador: " + jogadores[i].getId());
-			  		System.out.println("Tome seu dinheiro R$" + jogadores[i].getTotal());
-			  		break;
+		Boolean continua=true; 
+		while(continua){
+			continua=false;
+	    for(int i=0; i<jogadores.length; i++){
+				System.out.println("\nOpções do Jogador " + jogadores[i].getId());
+				System.out.println("(P)edir\n(F)icar\n(D)ividir\n(S)eguro\nF(i)nalizar\n");
+				char c = 'E'; // erro 
+				try{
+					if(jogadores[i].getTotal()==0)
+						c = 'I';
+					else
+						c = Character.toUpperCase((char)System.in.read());
+					switch(c){
+				  	case 'P': 
+				  		System.out.println("Nova carta: " + jogadores[i].pedirCarta().toString());
+				  		System.out.println("Valor da mão: " + jogadores[i].getMao().getValor());
+				  		continua=true;
+				  		break;
+				  	case 'F':
+				  		System.out.println("Valor da mão: " + jogadores[i].getMao().getValor());
+				  		break;		  		
+				  	case 'D':
+				  		break;
+				  	case 'S':
+				  		break;
+				  	case 'I':
+				  		System.out.println("Obrigado por participar Jogador: " + jogadores[i].getId());
+				  		System.out.println("Tome seu dinheiro R$" + jogadores[i].getTotal());
+				  		break;
+					}
+				}catch(IOException e){
+					e.printStackTrace();
 				}
-			}catch(IOException e){
-				e.printStackTrace();
-			}
-    }
+	    }
+		}
 	}
 	
   public void mostrarCartas(){
@@ -72,13 +77,26 @@ public class BlackJack implements UIJogador{
     		if(j!=maoJogador.size()-1) System.out.print(", ");
     	}
   		System.out.println("\nValor da mão: " + maoJogador.getValor());
+  		if(maoJogador.IsBlackJack()) System.out.println("***BLACKJACK***");
     }
+  }
+	
+  public void pagarApostas(){
+  	int vencedor = banca.pagarApostas();
+		System.out.print("O vencedor da rodada foi: ");
+		if(vencedor==-1){
+			System.out.println("a Banca");
+			System.out.println("Total da Banca R$" + banca.getTotal());
+		}	
+		else{
+			System.out.println("o jogador " + vencedor);
+			System.out.println("Total da jogador " + vencedor + " R$" + banca.getTotal());
+		}
   }
 	
 	public static void main(String[] args) {
   	final int numJogadores = 1;
     Jogador jogadores[] = new Jogador[numJogadores];
-
   	for(int i=0; i<jogadores.length; i++)
     	jogadores[i] = new Jogador(i); 
 
@@ -90,6 +108,6 @@ public class BlackJack implements UIJogador{
     banca.darCartas();
    	bj.mostrarCartas();
    	bj.fazerJogada();
-   
+    bj.pagarApostas();
  	}
 }
--- trunk/Integrante.java
+++ trunk/Integrante.java
@@ -1,10 +1,31 @@
 public class Integrante {
 	protected Mao mao = new Mao();
+	private double total = 0;
 
 	public Mao getMao(){
 	  return mao;	
 	}
+
+	public void setMao(Mao mao) {
+		this.mao = mao;
+	}
 	
+	public void addTotal(double valor){
+		total += valor;
+	}
+	
+	public double getTotal(){
+		return total;
+	}
+	
+	public void setTotal(double total) {
+		this.total = total;
+	}
+
+	public void limparMao(){
+		mao.clear();
+	}
+
 	public void ficar() {
 
 	}
